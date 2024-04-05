48bc81eb8c1b337de64d169497a3bd18fc558e40
fathanaliii <fathanali2310@gmail.com>
2024-04-05 10:51:01 +0700

    h

--- app/Views/dashboard/v_dashboard.php
+++ app/Views/dashboard/v_dashboard.php
@@ -95,7 +95,6 @@
     </div>
     <div class="row">
         <div class="col-lg-6 mb-4">
-            <!-- Project Card Example -->
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
                     <h6 class="m-0 font-weight-bold text-primary">INFORMATIONS</h6>
